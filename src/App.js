fe3f8ab567de1c2e3db2a989211ae5b9ae7ecba7
mazengh <mazengh@gmail.com>
2020-09-16 20:20:11 -0700

    added instructions and updated packages

--- src/App.js
+++ src/App.js
@@ -13,19 +13,26 @@ function App() {
         </Heading>
       </header>
       <main className='App-content'>
-        <p>
+        <h2 className='App-description'>
           A web accessible accordion component implemented based on the{' '}
           <a
-            href='https://www.w3.org/TR/wai-aria-practices-1.1/examples/accordion/accordion.html'
+            href='https://www.w3.org/TR/wai-aria-practices-1.2/examples/accordion/accordion.html'
             rel='noopener noreferrer'
             target='_blank'
           >
             W3 best practices
           </a>
           .
-        </p>
+        </h2>
+        <ul className='App-instructions'>
+          <li>Press <code>Enter/Space</code> to change the collapsed/expanded state of the associated panel</li>
+          <li>Press <code>Arrow Down</code> to move focus to the <strong>next</strong> accordion header</li>
+          <li>Press <code>Arrow Up</code> to move focus to the <strong>previous</strong> accordion header</li>
+          <li>Press <code>Home</code> to move Moves focus to the <strong>first</strong> accordion header</li>
+          <li>Press <code>End</code> to move Moves focus to the <strong>last</strong> accordion header</li>
+        </ul>
 
-        <Accordion classes='App-accordion-1' level={2} allowMultiple={false}>
+        <Accordion classes='App-accordion-1' level={3} allowMultiple={false}>
           {(accordionProps) => (
             <React.Fragment>
               <AccordionSection {...accordionProps} label='2020'>
